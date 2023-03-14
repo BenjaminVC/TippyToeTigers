4232677ad21714b4cb484506f04396bebbaeb2e3
Jonida Durbaku <durbaku.j@gmail.com>
2023-03-14 08:30:10 -0700

    Added the queries for creating tables and created database

--- README.md
+++
@@ -1,4 +0,0 @@
-# Tippy Toe Tigers 😼 CS 380
-<b>Web Programming final assignment</b><br>
-
-Group 3 a.k.a The Tippy Toe Tiger, Charles Biffin, Ben Van Cise, and Jonida Durbaku,  are developing a web interface that allows a user to play two games and track their high scores across each game. One game will be Snake and the other will be Hangman.The integrated development environment that we are choosing is Visual Studio Code and Docker for providing the server for PHP. SQLite will help us with storing necessary data for the games and users’ information.
--- database.sql
+++ database.sql
@@ -1,27 +1,74 @@
-CREATE DATABASE game_database;
-
-USE game_database;
-
-CREATE TABLE users (
-                       id INT PRIMARY KEY,
-                       username VARCHAR(50) NOT NULL,
-                       password VARCHAR(50) NOT NULL,
-                       email VARCHAR(100) NOT NULL
+CREATE TABLE user_info (
+    user_id INTEGER PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL
 );
 
-CREATE TABLE high_scores (
-                             id INT PRIMARY KEY,
-                             user_id INT,
-                             game_name VARCHAR(50) NOT NULL,
-                             score INT NOT NULL,
-                             date_played DATE NOT NULL,
-                             FOREIGN KEY (user_id) REFERENCES users(id)
+INSERT INTO user_info (user_id, first_name, last_name, email) VALUES 
+    (1, 'John', 'Doe', 'john.doe@example.com'),
+    (2, 'Jane', 'Doe', 'jane.doe@example.com'),
+    (3, 'Bob', 'Smith', 'bob.smith@example.com'),
+    (4, 'Charles', 'Biffin', 'charles.biffin@example.com'),
+    (5, 'Ben', 'Van Cise', 'ben.vancise@example.com'),
+    (6, 'Jonida', 'Durbaku', 'jonida.durbaku@example.com'),
+    (7, 'Ann', 'Williams', 'ann.williams@example.com'),
+    (8, 'James', 'Blunt', 'james.blunt@example.com'),
+    (9, 'Sam', 'Smith', 'sam.smith@example.com'),
+    (10, 'admin', 'admin', 'admin.admin@example.com');
+
+CREATE TABLE user_account (
+    account_id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    user_password TEXT NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES user_info(user_id)
 );
 
-CREATE TABLE game_data (
-                           id INT PRIMARY KEY,
-                           user_id INT,
-                           game_name VARCHAR(50) NOT NULL,
-                           data TEXT NOT NULL,
-                           FOREIGN KEY (user_id) REFERENCES users(id)
+INSERT INTO user_account (account_id, user_id, user_password) VALUES 
+    (1, 1, 'password123'),
+    (2, 2, 'password123'),
+    (3, 3, 'password123'),
+    (4, 4, 'password123'),
+    (5, 5, 'password123'),
+    (6, 6, 'password123'),
+    (7, 7, 'password123'),
+    (8, 8, 'password123'),
+    (9, 9, 'password123'),
+    (10, 10, 'password123');
+
+CREATE TABLE games (
+  game_id INTEGER PRIMARY KEY,
+  game_name TEXT
 );
+
+INSERT INTO games (game_id, game_name) VALUES (1, 'hangman'), (2, 'snake') ;
+
+CREATE TABLE categories (
+  category_id INTEGER PRIMARY KEY,
+  category_name TEXT
+);
+
+INSERT INTO categories (category_id, category_name) VALUES (1, 'Fruits'), (2, 'Animals'), (3, 'Countries');
+
+CREATE TABLE rounds (
+  round_id INTEGER PRIMARY KEY,
+  user_id INTEGER,
+  game_id INTEGER,
+  category_id INTEGER,
+  score INTEGER,
+  FOREIGN KEY (user_id) REFERENCES user_account(user_id),
+  FOREIGN KEY (game_id) REFERENCES games(game_id),
+  FOREIGN KEY (category_id) REFERENCES categories(category_id)
+);
+
+INSERT INTO rounds (user_id, game_id, category_id, score) VALUES 
+    (1, 1, 2, 100),
+    (3, 1, 2, 90),
+    (2, 2, 1, 75),
+    (4, 3, 3, 60),
+    (5, 2, 2, 80), 
+    (1, 3, 1, 85), 
+    (2, 1, 1, 70),
+    (3, 3, 2, 95),
+    (4, 2, 3, 50),
+    (5, 1, 1, 65);
