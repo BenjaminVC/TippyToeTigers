3822f3480d8b3f55d438956127795467881f6e87
Jonida Durbaku <durbaku.j@gmail.com>
2023-03-15 00:08:17 -0700

    moved some files to organize the project

--- Hangman.php
+++ Hangman.php
@@ -9,7 +9,7 @@ session_start();
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Hangman</title>
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet" />
-  <link rel="stylesheet" href="Hangman.css" />
+  <link rel="stylesheet" href="assets/Hangman.css" />
 </head>
 
 <body>
@@ -30,7 +30,7 @@ session_start();
       <button id="new-game-button">New Game</button>
     </div>
   </div>
-  <script src="Hangman.js"></script>
+  <script src="games/Hangman.js"></script>
 </body>
 
 </html>
