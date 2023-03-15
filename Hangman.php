227a23f4ec2bb0a2d1ea75343ca751cb8d8bbd88
Jonida Durbaku <durbaku.j@gmail.com>
2023-03-15 00:05:37 -0700

    Added functionalities for scores on hangman

--- Hangman.php
+++ Hangman.php
@@ -9,7 +9,7 @@ session_start();
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Hangman</title>
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet" />
-  <link rel="stylesheet" href="assets/Hangman.css" />
+  <link rel="stylesheet" href="Hangman.css" />
 </head>
 
 <body>
@@ -34,3 +34,5 @@ session_start();
 </body>
 
 </html>
+
+
