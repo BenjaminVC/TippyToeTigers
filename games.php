16fac47e8942a275f02046987dae8abd29264bd4
BenVC <benjamin.van-cise@bellevuecollege.edu>
2023-03-14 18:06:44 -0700

    added games.css and animation. changed games.php

--- games.php
+++ games.php
@@ -4,15 +4,16 @@ session_start();
 <html>
     <head>
         <title>Welcome</title>
-        <link rel="stylesheet" href="homePage.css">
+        <link rel="stylesheet" href="/assets/games.css">
     </head>
 
     <body>
         <p>Account: <?php echo $_SESSION['username']; ?> | logout</p>
-    	<h2>Choose your game!</h2>
-    	<ul>
-    		<button><a href="snake.php">Snake</a></button>
-    		<button><a href="hangman.php">Hangman</a></button>
-    	</ul>
+    	<div>
+            <h2>😼 Choose your game!</h2>
+    	
+    	    <button><a href="snake.php">Snake</a></button>
+    	    <button><a href="hangman.php">Hangman</a></button>
+        </div>
     </body>
 </html>
